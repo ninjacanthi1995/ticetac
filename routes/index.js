2dcdc7f382223a5109696b59bb699281ff1a7b39
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2021-06-25 13:06:25 +0200

    route to save user journeys && dynamise journs view

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,22 @@ var express = require("express");
 var router = express.Router();
 
 const journeyModel = require("../models/journeys");
+const userModel = require("../models/users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  if(req.session.user){
+    res.redirect("/home")
+  // }else{
+  //   req.session.user = await userModel.find({ email: "dorian@mail.com" })
+  //   req.session.user = req.session.user[0]
+  //   res.redirect('/home')
+  }
   res.render('index');
 });
 
 
 router.get("/home", async function (req, res) {
-  console.log(req.session.user);
   if (req.session.user) {
     res.render("home", { journeys: req.session.journeys });
   } else {
@@ -40,8 +47,18 @@ router.post("/push-journey", async (req, res) => {
   res.redirect('/tickets')
 });
 
-router.get('/tickets', async (req, res) => {
-  res.render('tickets', { tickets: req.session.tickets });
+router.get('/tickets', (req, res) => {
+  if(req.session.user){
+    res.render('tickets', { tickets: req.session.tickets });
+  }
+  res.redirect('/')
 });
+
+router.post('/journeys-to-db', async (req, res) => {
+  const user = await userModel.findById(req.session.user._id)
+  user.journeys.push(...JSON.parse(req.body.arrayid))
+  user.save()
+  res.redirect("/home")
+})
 
 module.exports = router;
